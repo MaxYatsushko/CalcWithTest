d1a0062846248e4feca0a68a43ac2d6cbce539c1
Maks <max@moais.ru>
2023-03-28 09:49:38 +0300

    Initial commit

---
+++ src/main/java/com/example/calcwithtest/CalcWithTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.calcwithtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcWithTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcWithTestApplication.class, args);
+	}
+
+}
